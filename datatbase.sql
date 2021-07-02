ec0bb408ebe96f47ba87fac8cd7c8fcfae912877
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-02 11:51:43 -0500

    added SQL file

---
+++ datatbase.sql
@@ -0,0 +1,8 @@
+CREATE TABLE koala (
+    "id" serial PRIMARY KEY,
+    "name" varchar(80),
+    "gender" varchar(10),
+    "age" int,
+    "readyForTransfer" varchar(10),
+    "notes" varchar (250)
+);
